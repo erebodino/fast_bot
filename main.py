aababd6af801226b2802b10586f6fb9ccaa3d183
erebodino <erebodino@gmail.com>
2024-04-29 18:09:55 -0300

    rename function

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ app = FastAPI()
 
 
 @app.post("/api/v1/message")
-def read_root(message:Message):
+def message(message:Message):
     return {"chat_id":message.chatId, "message":"The rest framework read the --{}--".format(message.messageText)}
